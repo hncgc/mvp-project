f59ce1fe4a172ceff7fdda1a376c4d9e53f65f8c
cgc <cgc@prosysoft.com>
2017-12-06 17:24:25 +0800

    update

---
+++ README.md
@@ -0,0 +1,21 @@
+# A MVP project
+
+---------------------
+> * 引导页，启动页，
+> * 一级页面框架
+> * 日志
+> * 数据库
+> * RX，net
+> * 动态权限申请
+> * 轮播图
+> * Vector兼容处理
+> * Eventbus
+> * MVP
+> * ZLoading
+> * Fragment与Activity通讯
+> * AbPullToRefreshView
+> * SmartRefreshLayout智能下拉刷新上拉加载框架
+> * TabLayout，ViewPager，嵌套Fragment滑动切换
+> * 产品列表MVP
+
+-----------------------
--- app/src/main/java/com/pccb/newapp/ui/ConsultantFragment.java
+++ app/src/main/java/com/pccb/newapp/ui/ConsultantFragment.java
@@ -3,12 +3,17 @@ package com.pccb.newapp.ui;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pccb.newapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -20,14 +25,8 @@ import com.pccb.newapp.R;
  * create an instance of this fragment.
  */
 public class ConsultantFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,16 +34,15 @@ public class ConsultantFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment ConsultantFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ConsultantFragment newInstance() {
         ConsultantFragment fragment = new ConsultantFragment();
         return fragment;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
     @Override
@@ -60,22 +58,24 @@ public class ConsultantFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_consultant, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        // App Logo
+        //toolbar.setLogo(R.mipmap.ic_launcher);
+        // Title
+        //toolbar.setTitle("投资顾问");
+        // Sub Title
+        //toolbar.setSubtitle("Sub title");
+        //setSupportActionBar(toolbar);
+    }
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
     }
 
     @Override
@@ -84,18 +84,8 @@ public class ConsultantFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+       void onFragmentInteraction(Uri uri);
     }
 }
